5cfa31bf0bdd83814bb0a00a6535c7f60f69eaf1
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-08-17 23:29:59 +0530

    Add files via upload
    use state property to add dynamic content
    update data when button is clicked

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Nupoor', age: '28' },
+      { name: 'Manu', age: '29'},
+      { name: 'Stephanie', age: '26'}
+    ] 
+  };
+
+  switchNameHandler = () => {
+    // console.log('This was clicked');
+    // this.state.people[1].name = 'Ariji'; // don't change mutable CONST
+    this.setState({people: [
+        { name: 'Nupoor', age: '28' },
+        { name: 'Manu', age: '29'},
+        { name: 'Stephanie', age: '30'}
+      ] 
+    }) ; // method to update state property and react will come to know what to update
+  };
+
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>Paragraph is working too !!</p>
-        <Person name="Nupoor" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Cooking </Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Cooking </Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
 
