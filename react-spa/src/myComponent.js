252f00b4b34310a2d8d0d7b5c4d9eba9c76013b6
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-12-25 15:01:52 +0530

    stateless component
    stateless components example

--- react-spa/src/myComponent.js
+++ react-spa/src/myComponent.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Sample extends Component {
-    render() {
-        const { kids } = this.props;
-        const kidsList = kids.map(kid => {
-            return (
-                <div className="my-app" key={kid.id} >
-                    <p>{kid.id} : My name is { kid.name }, I'm { kid.age } years old and I'm from { kid.city } </p>
-                </div>
-            )
-        })
+const Sample = ({ kids }) => {
+    const kidsList = kids.map(kid => {
         return (
-            <div className="kids-list">
-                { kidsList }
+            <div className="my-app" key={kid.id} >
+                <p>{kid.id} : My name is { kid.name }, I'm { kid.age } years old and I'm from { kid.city } </p>
             </div>
         )
-        
-    }
+    })
+    return (
+        <div className="kids-list">
+            { kidsList }
+        </div>
+    )
 }
+ 
 
 export default Sample;
