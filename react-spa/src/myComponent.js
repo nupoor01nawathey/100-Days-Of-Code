c21f9e44fd5efd7db472ae99129080fe308cc54f
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-12-27 10:22:52 +0530

    delete kid
    delete kid

--- react-spa/src/myComponent.js
+++ react-spa/src/myComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Sample = ({ kids }) => {
+const Sample = ({ kids, deleteKid }) => {
     const kidsList = kids.map(kid => {
         return (
             <div className="my-app" key={kid.id} >
                 <p>{kid.id} : My name is { kid.name }, I'm { kid.age } years old and I'm from { kid.city } </p>
+                <button onClick={() => {deleteKid(kid.id)}}>Delete</button>
+                <hr />
             </div>
         )
     })
